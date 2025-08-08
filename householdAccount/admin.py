3ce6aeed078c3aa184b95937466d81f69a0d9910
Kryoya3926 <ryoya.kuri3926@gmail.com>
2025-08-08 08:52:35 +0000

    add_book

--- householdAccount/admin.py
+++ householdAccount/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Category, AccountBook
 admin.site.register(AccountBook)
 admin.site.register(Category)
--- householdAccount/urls.py
+++ householdAccount/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name='householdAccount'
+app_name='book'
 urlpatterns=[
     path('', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/add/', views.BookCreateView.as_view(), name='book_add'),
 ]

2d6269f86f9fc4f5f8ade5505d800391e89f5711
Kryoya3926 <ryoya.kuri3926@gmail.com>
2025-08-27 08:58:13 +0000

    delete book

---
+++ householdAccount/templatetags/blog_extras.py
@@ -0,0 +1,16 @@
+from django import template
+from django.shortcuts import resolve_url
+from urllib import parse
+
+register=template.Library()
+
+@register.simple_tag
+def get_return_link(request):
+    top_page=resolve_url('book:book_list')
+    referer=request.environ.get('HTTP_REFERER')
+
+    if referer:
+        parse_result=parse.urlparse(referer)
+        if request.get_host()==parse_result.netloc: #URLをhttps://www.example.com/path?query=1#fragment とすると、parse_result.netlocはwww.example.comの部分となる。
+            return referer
+    return top_page
